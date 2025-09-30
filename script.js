926169797fbb4f8dc371dd5b6a12045336e27d7a
Matheus <um.dois.quase.tres@gmail.com>
2025-09-29 21:24:24 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navigation = document.querySelector('.navigation');
+
+    menuToggle.addEventListener('click', function() {
+        navigation.classList.toggle('active');
+    });
+});
