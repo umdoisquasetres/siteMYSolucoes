8e96466fd6e02740c07fc018ea6aa9ff9dd00312
Matheus <um.dois.quase.tres@gmail.com>
2025-09-29 22:55:53 -0300

    feat: Refine visual design with new color palette and styles

--- script.js
+++ script.js
@@ -43,4 +43,26 @@ document.addEventListener('DOMContentLoaded', function() {
     changeLinkState();
 
     window.addEventListener('scroll', changeLinkState);
+
+    // Scroll animation
+    const fadeInSections = document.querySelectorAll('.fade-in-section');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    fadeInSections.forEach(section => {
+        observer.observe(section);
+    });
 });
